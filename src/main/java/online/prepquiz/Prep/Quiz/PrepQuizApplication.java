bbf7bd68b9b057fa2cc87f8718c2c792e71a41db
Ankit Raj <radheankit110031@gmail.com>
2026-07-14 16:44:39 +0530

    initial commit

---
+++ src/main/java/online/prepquiz/Prep/Quiz/PrepQuizApplication.java
@@ -0,0 +1,13 @@
+package online.prepquiz.Prep.Quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepQuizApplication.class, args);
+	}
+
+}
